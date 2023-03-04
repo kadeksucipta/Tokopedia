3b1b3df28bad84369662792a1bbf342273749c75
kadek12345 <kadek.sucipta005@gmail.com>
2023-03-04 20:27:08 +0800

    cloning tokopedia

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function on() {
+    var menuBar = document.getElementById("menubar")
+
+    var overlay = document.getElementById("overlay")
+
+    menuBar.classList.toggle("menuBarStyle")
+    overlay.classList.toggle("overlayStyle")
+}
+
+function kategoryOn() {
+    var kategory = document.getElementById("kategory")
+
+    kategory.classList.toggle("kategoryStyle")
+}
